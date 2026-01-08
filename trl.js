5ccee31a80d48b8bd2201776493aa26d06d9f702
diosa2074-commits <diosa2074@gmail.com>
2026-01-08 13:19:42 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1458795213928661145/EmXZJu7YDuH9N3vjLIGb5FKcMvKUUyQBOpZ5lKIyFTK2-1454aw6s4zWnTjthnth7fkv'; // Reemplaza esto con tu Webhook real
